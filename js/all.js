b2c96dde132830a80937381e7f0267b941536cd9
RAY <loveyou520babytw@gmail.com>
2019-01-10 18:30:09 +0800

    DOM取得方式錯誤

--- js/all.js
+++ js/all.js
@@ -1,34 +1,36 @@
-const aifiMoneyID = document.getElementById('aifiMoney').value;
-const percentageID = document.getElementById('percentage').value;
+const aifiMoneyID = document.getElementById('aifiMoney');
+const percentageID = document.getElementById('percentage');
 const calculateID = document.getElementById('calculate');
 const fromListID = document.getElementById('fromList');
+
 function calculateTotalAifi(e) {
     e.preventDefault();
     const appAifianID = document.getElementById('appAifian');
     str = '';
     const dayYears = 365;
-    const aifiPoint = percentageID / 100;
-    const day365Money = (aifiMoneyID * aifiPoint);
-    const day7Money = (day365Money / dayYears * 7);
-    let strList = {
-        day365Money: day365Money,
-        day7Money: day7Money,
+    const aifiPoint = percentageID.value / 100;
+    const day365Money = aifiMoneyID.value * aifiPoint;
+    const day7Money = day365Money / dayYears * 7;
+    const strList = {
+        day365Money: day365Money.toFixed(2),
+        day7Money: day7Money.toFixed(2),
         realMoney: Math.round(day7Money)
     }
     str = `
         <p>一年收益(365天)
-            <em id="result365">${day365Money.toFixed(2)}</em>元
+            <em id="result365">${strList.day365Money}</em>元
         </p>
         <p>本週回饋金
-            <em id="result7">${day7Money.toFixed(2)}</em>元
+            <em id="result7">${strList.day7Money}</em>元
         </p>
         <p>實際取得金額
-            <em id="resultAifi">${Math.round(day7Money)}</em>元
+            <em id="resultAifi">${strList.realMoney}</em>元
         </p>
     `;
     localStorage.setItem('aifianList', JSON.stringify(strList));
     
     appAifianID.innerHTML = str;
+    document.getElementById('fromList').reset();
 }
 
 
