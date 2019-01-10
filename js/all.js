0ab96a510ad9f8007b19bc90e8114284d1d6bb8f
RAY <loveyou520babytw@gmail.com>
2019-01-10 18:53:46 +0800

    針對localstorage修正

--- js/all.js
+++ js/all.js
@@ -3,10 +3,11 @@ const percentageID = document.getElementById('percentage');
 const calculateID = document.getElementById('calculate');
 const fromListID = document.getElementById('fromList');
 
+let aifianArrayList = JSON.parse(localStorage.getItem('aifianList') || [])
+loaclIndex(aifianArrayList);
 function calculateTotalAifi(e) {
     e.preventDefault();
     const appAifianID = document.getElementById('appAifian');
-    str = '';
     const dayYears = 365;
     const aifiPoint = percentageID.value / 100;
     const day365Money = aifiMoneyID.value * aifiPoint;
@@ -16,6 +17,7 @@ function calculateTotalAifi(e) {
         day7Money: day7Money.toFixed(2),
         realMoney: Math.round(day7Money)
     }
+    let str = '';
     str = `
         <p>一年收益(365天)
             <em id="result365">${strList.day365Money}</em>元
@@ -27,9 +29,31 @@ function calculateTotalAifi(e) {
             <em id="resultAifi">${strList.realMoney}</em>元
         </p>
     `;
-    localStorage.setItem('aifianList', JSON.stringify(strList));
+    aifianArrayList.push(strList);
+    localStorage.setItem('aifianList', JSON.stringify(aifianArrayList));
     appAifianID.innerHTML = str;
+    loaclIndex(aifianArrayList);
     document.getElementById('fromList').reset();
+}
+
+function loaclIndex(item){
+    let localAifianListID = document.getElementById('localAifianList');
+    console.log('localAifianListID');
+    let str ='';
+    let itemLen = item.length;
+    item.forEach(data => {
+        str += `
+        <p>一年收益(365天)
+            <em id="result365">${data.day365Money}</em>元
+        </p>
+        <p>本週回饋金
+            <em id="result7">${data.day7Money}</em>元
+        </p>
+        <p>實際取得金額
+            <em id="resultAifi">${data.realMoney}</em>元
+        </p>`;
+    });
+    localAifianListID.innerHTML = str;
 }
 
 calculateID.addEventListener('click', calculateTotalAifi);
